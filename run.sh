c7e96264eee8fdac2207a27cdb64b75be680f4b5
Vedant-mac <your.email@example.com>
2025-11-25 17:58:13 -0800

    init

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE="$1"
+
+if [[ "$FILE" == *.cpp ]]; then
+    g++ "$FILE" -O2 -std=c++17 -o run.out
+    ./run.out < input.txt
+elif [[ "$FILE" == *.py ]]; then
+    python3 "$FILE" < input.txt
+else
+    echo "Unsupported file type"
+fi
